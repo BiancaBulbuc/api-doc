b07c780b58563fd7239d6c7b3eac4574a34228fc
Håkan Nylén <confacted@gmail.com>
2016-11-27 22:18:38 +0100

    fixed vagrant box

--- script/vagrant.sh
+++ script/vagrant.sh
@@ -2,8 +2,14 @@
 
 sudo locale-gen en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -yq ruby ruby-dev build-essential git
-sudo gem install --no-ri --no-rdoc bundler
-cd /vagrant/ %% bundle install
-bundle exec middleman server --watcher-force-polling --watcher_latency=1 &> ~/middleman.log &
+sudo apt-get update -y
+
+su - vagrant -c 'command curl -sSL https://rvm.io/mpapis.asc | gpg --import -'
+su - vagrant -c 'curl -sSL https://get.rvm.io | bash -s stable'
+su - vagrant -c 'rvm install 2.2.5'
+su - vagrant -c 'rvm install 2.2.5'
+su - vagrant -c 'rvm use 2.2.5 --default'
+su - vagrant -c 'gem install bundler'
+su - vagrant -c 'cd /vagrant && bundle install'
+
+cd /vagrant && bundle exec middleman server --watcher-force-polling --watcher_latency=1 &> ~/middleman.log &
